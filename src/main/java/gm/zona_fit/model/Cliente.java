367fa0361c390c579549917e5e602b7c1b4fbe80
Carlos Verdugo <carlos.versa99@gmail.com>
2026-08-25 16:22:26 -0700

    Adding client entity

---
+++ src/main/java/gm/zona_fit/model/Cliente.java
@@ -0,0 +1,28 @@
+package gm.zona_fit.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+    private String apellido;
+    private Integer membresia;
+
+}
