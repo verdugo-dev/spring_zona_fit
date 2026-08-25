443e8b63bca56596a000664f13d363c2df9a1f49
Carlos Verdugo <carlos.versa99@gmail.com>
2026-08-25 15:57:30 -0700

    Initial commit

---
+++ src/main/java/gm/zona_fit/ZonaFitApplication.java
@@ -0,0 +1,13 @@
+package gm.zona_fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonaFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZonaFitApplication.class, args);
+	}
+
+}
